ba0aac703dbc3f09179b3795312d92228d70cf7d
Madison Evans <m.corbinevans@gmail.com>
2023-02-06 01:01:14 -0700

    navbar coming into place

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
 import "./App.css";
-import SkillCard from "./components/SkillCard/SkillCard";
 import SkillCardGroup from "./components/SkillCardGroup/SkillCardGroup";
-const frontendDescription =
-	"A good software engineer's frontend skills include expertise in HTML, CSS, and JavaScript, as well as experience with front-end frameworks such as React, Angular, or Vue. They should also have an eye for design and user experience, as well as knowledge of web performance and accessibility.";
-
-const skillsArray = [
-	{
-		cardTitle: "Frontend",
-		skillList: ["Javascript", "React", "HTML/CSS/SCSS"],
-		description: "this is where we will describe our frontend skills",
-	},
-	{
-		cardTitle: "Backend",
-		skillList: ["Python", "Java", "Django", "Flask"],
-		description: "this is where we will describe our backend skills",
-	},
-	{
-		cardTitle: "Other",
-		skillList: ["SolidWorks", "Scitkit Learn", "Ableton"],
-		description: "this is where we will describe any of our other skills",
-	},
-];
+import Navigation from "./components/Navigation/Navigation";
+//TODO: use skillsArray for skillCardGroup Prop
+// const skillsArray = [
+// 	{
+// 		cardTitle: "Frontend",
+// 		skillList: ["Javascript", "React", "HTML/CSS/SCSS"],
+// 		description: "this is where we will describe our frontend skills",
+// 	},
+// 	{
+// 		cardTitle: "Backend",
+// 		skillList: ["Python", "Java", "Django", "Flask"],
+// 		description: "this is where we will describe our backend skills",
+// 	},
+// 	{
+// 		cardTitle: "Other",
+// 		skillList: ["SolidWorks", "Scitkit Learn", "Ableton"],
+// 		description: "this is where we will describe any of our other skills",
+// 	},
+// ];
 
 function App() {
 	return (
 		<>
-			<SkillCardGroup skillsArray={skillsArray} />
+			<Navigation />
 		</>
 	);
 }
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Navigation.css";
+const Navigation = () => {
+	return (
+		<div className="navigation-container">
+			<h1>item1</h1>
+			<h1>item2</h1>
+			<h1>item3</h1>
+			<h1>item4</h1>
+		</div>
+	);
+};
+
+export default Navigation;

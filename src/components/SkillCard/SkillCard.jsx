41ebd4e3e609319bc61b76160abfc7e9265e9cc4
Madison Evans <m.corbinevans@gmail.com>
2023-02-06 01:01:14 -0700

    hidden toggle feature added

--- src/components/SkillCard/SkillCard.jsx
+++ src/components/SkillCard/SkillCard.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import "./SkillCard.css";
+import { useState } from "react";
 
 const SkillCard = ({ cardTitle, skillList }) => {
+	const [isHidden, setIsHidden] = useState(true);
+	function toggleHidden() {
+		setIsHidden((prevState) => !prevState);
+	}
 	return (
-		<div className="skillcard-container">
+		<div className="skillcard-container" onClick={toggleHidden}>
 			<h2>{cardTitle}</h2>
 			<ul>
 				{skillList.map((skill, index) => {
 					return <li key={index}>{skill}</li>;
 				})}
 			</ul>
-			<p>
+			<p className={isHidden ? "hidden" : ""}>
 				Lorem ipsum dolor sit, amet consectetur adipisicing elit. At ad et ut ab
 				ratione nesciunt libero quas, expedita eius tempora? Facilis nulla ipsam
 				numquam temporibus autem? Nisi, placeat ratione fugit libero rem
